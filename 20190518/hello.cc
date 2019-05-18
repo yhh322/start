fe0626add3d3936b2400a3c094b7713ed713b0c7
haohb13 <haohb13@gmail.com>
2019-05-18 11:05:07 +0800

    lwh:add sub directory 20190518

---
+++ 20190518/hello.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    hello.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2019-05-18 11:04:04
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+ 
+int main(void)
+{
+	cout << "hello,world" << endl;
+	return 0;
+}
