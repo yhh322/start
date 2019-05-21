dd4e56ae0a0fe9bb0802a46fa00609822c665735
haohb13 <haohb13@gmail.com>
2019-05-21 11:50:21 +0800

    lwh:add a few files in 20190521

---
+++ 20190521/pimpl/Line.h
@@ -0,0 +1,31 @@
+ ///
+ /// @file    Line.h
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2019-05-21 11:04:46
+ ///
+ 
+#ifndef __LINE_H__
+#define __LINE_H__
+
+
+class Line
+{//作为接口存在
+ //PIMPL设计模式
+ //
+ //优势: 1. 实现信息隐藏
+ //		 2. 接口稳定，可以以最小的代价，实现库的平滑升级
+ //		 3. 减小编译依赖，提高编译速度
+public:
+	Line(int, int, int, int);
+	~Line();
+
+	void printLine() const;
+	
+	class LineImpl;//类的前向声明, 嵌套类(内部类)
+private:
+	LineImpl * _pimpl;
+};
+
+ 
+
+#endif
---
+++ 20190521/singleton/Singleton.cc
@@ -0,0 +1,64 @@
+ ///
+ /// @file    Singleton.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2019-05-21 11:27:15
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//自动释放方法一: 嵌套类 + 静态对象
+
+class Singleton
+{
+	class AutoRelease
+	{
+	public:
+		AutoRelease()
+		{	cout << "AutoRelease()" << endl;	}
+
+		~AutoRelease()
+		{
+			cout << "~AutoRelease()" << endl;	
+			if(_pInstance)
+				delete _pInstance;
+		}
+	};
+public:
+	static Singleton * getInstance()
+	{
+		if(_pInstance == nullptr)
+			_pInstance = new Singleton();
+		return _pInstance;
+	}
+
+	void print() const {	cout << "print()" << endl;	}	
+
+	static void destroy()
+	{
+		if(_pInstance)
+			delete _pInstance;
+	}
+
+private:
+	Singleton() {	cout << "Singleton()" << endl;	}
+	~Singleton() {	cout << "~Singleton()" << endl;	}
+
+	friend class AutoRelease;
+private:
+	static Singleton * _pInstance;
+	static AutoRelease _autoRelease;
+};
+
+
+Singleton::AutoRelease Singleton::_autoRelease;
+Singleton * Singleton::_pInstance = nullptr; 
+
+int main(void)
+{
+	Singleton * p = Singleton::getInstance();
+	p->print();
+
+	return 0;
+}
